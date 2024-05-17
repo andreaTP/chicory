41bd77c4bac4fc3f75fa3fa2bfa9a3fe50dd8b95
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-05-17 19:18:46 +0100

    more specs

--- runtime/src/main/java/com/dylibso/chicory/runtime/Memory.java
+++ runtime/src/main/java/com/dylibso/chicory/runtime/Memory.java
@@ -335,4 +335,8 @@ public final class Memory {
     public void drop(int segment) {
         dataSegments[segment] = PassiveDataSegment.EMPTY;
     }
+
+    public DataSegment[] dataSegments() {
+        return dataSegments;
+    }
 }
