219283ae19b7dae8c97d48e3d2c8fd7a12d0ebcb
andreatp <andrea.peruffo1982@gmail.com>
2026-02-06 11:28:53 +0000

    Working reduced input

--- runtime/src/main/java/com/dylibso/chicory/runtime/MemCopyWorkaround.java
+++ runtime/src/main/java/com/dylibso/chicory/runtime/MemCopyWorkaround.java
@@ -8,57 +8,16 @@ package com.dylibso.chicory.runtime;
 public final class MemCopyWorkaround {
     private MemCopyWorkaround() {}
 
-    private interface MemoryCopyFunc {
-        void apply(int destination, int offset, int size, Memory memory);
-    }
-
     public static boolean shouldUseMemWorkaround() {
-        return shouldUseMemWorkaround(System.getProperty("java.version"));
+        return false;
     }
 
     public static boolean shouldUseMemWorkaround(String version) {
-        if (version == null || version.equals("0")) {
-            // Android https://developer.android.com/reference/java/lang/System#getProperties()
-            return false;
-        } else if (version.startsWith("1.")) {
-            // Java 8 or earlier: "1.8.0_231" → 8
-            return true;
-        } else {
-            // Java 9 or later: "11.0.9" or "17.0.1"
-            int dotIndex = version.indexOf(".");
-            String majorStr = (dotIndex != -1) ? version.substring(0, dotIndex) : version;
-            int dashIndex = majorStr.indexOf("-");
-            majorStr = (dashIndex != -1) ? majorStr.substring(0, dashIndex) : majorStr;
-            try {
-                return Integer.parseInt(majorStr) <= 17;
-            } catch (NumberFormatException nfe) {
-                return false;
-            }
-        }
+        return false;
     }
-
-    static {
-        if (shouldUseMemWorkaround()) {
-            MemoryCopyFunc noop1 = (destination, offset, size, memory) -> {};
-            MemoryCopyFunc noop2 = (destination, offset, size, memory) -> {};
-
-            // Warm up the JIT... to make it see memoryCopyFunc.apply is megamorphic
-            for (int i = 0; i < 1000; i++) {
-                memoryCopyFunc = noop1;
-                MemCopyWorkaround.memoryCopy(0, 0, 0, null);
-                memoryCopyFunc = noop2;
-                MemCopyWorkaround.memoryCopy(0, 0, 0, null);
-            }
-
-            memoryCopyFunc =
-                    (destination, offset, size, memory) -> memory.copy(destination, offset, size);
-        }
-    }
-
-    static MemoryCopyFunc memoryCopyFunc;
 
     public static void memoryCopy(int destination, int offset, int size, Memory memory) {
-        memoryCopyFunc.apply(destination, offset, size, memory);
+        memory.copy(destination, offset, size);
     }
 
     public static int i32_ge_u(int a, int b) {
